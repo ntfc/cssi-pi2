870397da5211adf2ac8a0176c2c2c3a716fd9cf4
miltonnunes52 <milton.nunes52@gmail.com>
2013-04-01 18:51:25 +0100

    Distributions added

--- src/test/Tag.java
+++ src/test/Tag.java
@@ -5,6 +5,8 @@
 
 package test;
 
+import java.util.BitSet;
+
 /**
  *
  * @author nc
@@ -19,12 +21,36 @@ public class Tag {
     this.securityParameter = securityParam;
   }
 
-  public void genR() {
+  //estará isto certo? A estar talvez devessemos repensar a ideia de usarmos bitset no polinomio
+  public double[] genE(PolynomialF2 p, double t) {
     // TODO
+    BitSet bs = p.getCoeficients();
+    double[] e = new double[bs.length()];
+    
+    for(int i = 0; i<bs.length(); i++){
+      if(bs.get(i) == true)
+        e[i] = t;
+      else e[i] = 1-t;
+    }
+    
+    return e;
   }
-  public void genE() {
+  
+  //será probability mass function ou comulative distribution function? Usei a 2ª
+  //http://en.wikipedia.org/wiki/Uniform_distribution_(discrete)
+  public double[] genR(PolynomialF2 p) {
     // TODO
+    BitSet bs = p.getCoeficients();
+    double[] r = new double[bs.length()];
+    
+    for(int i = 0; i<bs.length(); i++){
+      int val = bs.get(i) ? 1 : 0;//bool to int
+      r[i] = (val - 0 + 1)/(1 - 0 + 1);
+    }
+    
+    return r;
   }
+  
   public void calcZ() {
     // TODO: z = r * (s * mapping(c) + s') + e
   }
